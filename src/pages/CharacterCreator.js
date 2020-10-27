ba95994d6d19e06777adf3363941f803b8baa80c
OliverZ97 <oliver@mordoor.de>
2020-10-27 21:08:40 +0100

    style of input elements

--- src/pages/CharacterCreator.js
+++ src/pages/CharacterCreator.js
@@ -10,34 +10,80 @@ class MonsterCreator extends Component {
 
         return (
             <div className="d-flex flex-row">
-                <div className="d-flex flex-row">
-                    <label for="name">Vorname</label>
-                    <input className="input" name="name" type="text"></input>
-                    <label for="name">Nachname</label>
-                    <input className="input" name="name" type="text"></input>
+                <div className="card">
+                    <div className="d-flex flex-column">
+                        <div className="d-flex flex-row justify-content-around">
+                            <div className="input-label">
+                                <label htmlFor="name">Vorname</label>
+                                <input className="input" name="name" type="text"></input>
+                            </div>
+                            <div className="input-label">
+                                <label htmlFor="name">Nachname</label>
+                                <input className="input" name="name" type="text"></input>
+                            </div>
+                        </div>
+                        <div className="d-flex flex-row justify-content-around">
+                            <div className="input-label">
+                                <label htmlFor="race">Rasse</label>
+                                <select className="input" name="race">
+                                    <option>Elf</option>
+                                    <option>Gnom</option>
+                                    <option>Halbling</option>
+                                    <option>Mensch</option>
+                                    <option>Zwerg</option>
+                                </select>
+                            </div>
+                            <div className="input-label">
+                                <label htmlFor="class">Klasse</label>
+                                <select className="input" name="class">
+                                    <option>Barbar</option>
+                                    <option>Krieger</option>
+                                    <option>Magier</option>
+                                </select>
+                            </div>
+                        </div>
+                    </div>
                 </div>
 
-                <div className="d-flex flex-column">
-                    <div className="d-flex flex-row">
-                        <label for="ST">ST</label>
-                        <input name="ST" type="number" min="1" max="100"></input>
-                        <label for="GS">GS</label>
-                        <input name="GS" type="number" min="1" max="100"></input>
+
+                <div className="card">
+                    <div className="d-flex flex-column">
+                        <div className="d-flex flex-row justify-content-around">
+                            <div className="input-label">
+                                <label htmlFor="ST">ST</label>
+                                <input className="input" name="ST" type="number" min="1" max="100"></input>
+                            </div>
+                            <div className="input-label">
+                                <label htmlFor="GS">GS</label>
+                                <input className="input" name="GS" type="number" min="1" max="100"></input>
+                            </div>
+                        </div>
+                        <div className="d-flex flex-row justify-content-around">
+                            <div className="input-label">
+                                <label htmlFor="GW">GW</label>
+                                <input className="input" name="GW" type="number" min="1" max="100"></input>
+                            </div>
+                            <div className="input-label">
+                                <label htmlFor="KO">KO</label>
+                                <input className="input" name="KO" type="number" min="1" max="100"></input>
+                            </div>
+                        </div>
+                        <div className="d-flex flex-row justify-content-around">
+                            <div className="input-label">
+                                <label htmlFor="IN">IN</label>
+                                <input className="input" name="IN" type="number" min="1" max="100"></input>
+                            </div>
+                            <div className="input-label">
+                                <label htmlFor="ZT">ZT</label>
+                                <input className="input" name="ZT" type="number" min="1" max="100"></input>
+                            </div>
+                        </div>
                     </div>
-                    <div className="d-flex flex-row">
-                        <label for="GW">GW</label>
-                        <input name="GW" type="number" min="1" max="100"></input>
-                        <label for="KO">KO</label>
-                        <input name="KO" type="number" min="1" max="100"></input>
-                    </div>
-                    <div className="d-flex flex-row">
-                        <label for="IN">IN</label>
-                        <input name="IN" type="number" min="1" max="100"></input>
-                        <label for="ZT">ZT</label>
-                        <input name="ZT" type="number" min="1" max="100"></input>
-                    </div>
+
                 </div>
+
             </div>
+
         );
     }
 }
