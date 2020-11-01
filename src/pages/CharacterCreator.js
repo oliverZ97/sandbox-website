65c7adedc5bb755dacb5b2b81187757b97f680b5
OliverZ97 <oliver@mordoor.de>
2020-11-01 18:14:23 +0100

    fix dynamic state changes

--- src/pages/CharacterCreator.js
+++ src/pages/CharacterCreator.js
@@ -19,11 +19,20 @@ class CharacterCreator extends Component {
                 ko: 1,
                 in: 1,
                 zt: 1
+            },
+            character_info: {
+                prename: "",
+                name: "",
+                level: "1",
+                gender: "",
+                race: "",
+                class: ""
             }
         }
 
         this.setCharacterData = this.setCharacterData.bind(this);
-        this.getRandom = this.getRandom.bind(this);
+        this.updateStats = this.updateStats.bind(this);
+        this.handleChange = this.handleChange.bind(this);
     }
 
     async getFormularData() {
@@ -56,21 +65,53 @@ class CharacterCreator extends Component {
         }
     }
 
-    getRandom(dice) {
-        return Math.ceil(Math.random() * dice)
-    }
-
     rollBasis(dice) {
         let basis = Object.keys(this.state.base)
         let newBase = {}
         basis.forEach((att) => {
-            let roll = this.getRandom(dice);
+            let roll = c.getRandom(dice);
             newBase[att] = roll;
         })
         this.setState({
             base: newBase
         })
     }
+
+    handleChange(event) {
+        let base = this.state.base;
+        let name = event.target.name.toLowerCase();
+        base[name] = event.target.value;
+        this.setState({base: base});
+        this.updateStats();
+    }
+        
+   
+    updateStats() {
+        let data = document.querySelectorAll("input, select");
+        let state = {...this.state};
+        data.forEach((stat) => {
+            if(stat.nodeName === "INPUT") {
+                let value = stat.value;
+                let name = stat.name.toLowerCase();
+                if(name === "st" || name === "gs" || name === "gw" || name === "ko" || name === "in" || name === "zt") {
+                    state.base[name] = value;
+                } else {
+                    state.character_info[name] = value;
+                }
+            } else if(stat.nodeName === "SELECT") {
+                let value = stat.options[stat.selectedIndex].value.toLowerCase();
+                let name = stat.name;
+                state.character_info[name] = value;
+            } else {
+            }
+        })
+        this.setState({
+            base: state.base,
+            character_info: state.character_info
+        });
+    }
+
+
 
     render() {
         if (this.state.initial) {
@@ -95,8 +136,8 @@ class CharacterCreator extends Component {
                         <div className="d-flex flex-column">
                             <div className="d-flex flex-row justify-content-around">
                                 <div className="input-label">
-                                    <label htmlFor="name">Vorname</label>
-                                    <input className="input" name="name" type="text" placeholder="Eorn"></input>
+                                    <label htmlFor="prename">Vorname</label>
+                                    <input className="input" name="prename" type="text" placeholder="Eorn"></input>
                                 </div>
                                 <div className="input-label">
                                     <label htmlFor="name">Nachname</label>
@@ -112,19 +153,19 @@ class CharacterCreator extends Component {
                                 </div>
                                 <div className="input-label">
                                     <label htmlFor="level">Grad</label>
-                                    <input className="input" name="level" type="number" placeholder="1" min="0"></input>
+                                    <input className="input" name="level" type="number" placeholder="1" min="0" onInput={this.updateStats}></input>
                                 </div>
                             </div>
                             <div className="d-flex flex-row justify-content-around">
                                 <div className="input-label">
                                     <label htmlFor="race">Rasse</label>
-                                    <select className="input" name="race">
+                                    <select className="input" name="race" onChange={this.updateStats}>
                                         {races}
                                     </select>
                                 </div>
                                 <div className="input-label">
                                     <label htmlFor="class">Klasse</label>
-                                    <select className="input" name="class">
+                                    <select className="input" name="class" onChange={this.updateStats}>
                                         {classes}
                                     </select>
                                 </div>
@@ -138,31 +179,31 @@ class CharacterCreator extends Component {
                                 <div className="d-flex flex-row justify-content-around">
                                     <div className="input-label">
                                         <label htmlFor="ST">ST</label>
-                                        <input className="base input-sm" name="ST" type="number" min="1" max="100" value={this.state.base.st}></input>
+                                        <input className="base input-sm" name="ST" type="number" min="1" max="100" value={this.state.base.st} onChange={this.handleChange}></input>
                                     </div>
                                     <div className="input-label">
                                         <label htmlFor="GS">GS</label>
-                                        <input className="base input-sm" name="GS" type="number" min="1" max="100" value={this.state.base.gs}></input>
+                                        <input className="base input-sm" name="GS" type="number" min="1" max="100" value={this.state.base.gs} onChange={this.handleChange}></input>
                                     </div>
                                 </div>
                                 <div className="d-flex flex-row justify-content-around">
                                     <div className="input-label">
                                         <label htmlFor="GW">GW</label>
-                                        <input className="base input-sm" name="GW" type="number" min="1" max="100" value={this.state.base.gw}></input>
+                                        <input className="base input-sm" name="GW" type="number" min="1" max="100" value={this.state.base.gw} onChange={this.handleChange}></input>
                                     </div>
                                     <div className="input-label">
                                         <label htmlFor="KO">KO</label>
-                                        <input className="base input-sm" name="KO" type="number" min="1" max="100" value={this.state.base.ko}></input>
+                                        <input className="base input-sm" name="KO" type="number" min="1" max="100" value={this.state.base.ko} onChange={this.handleChange}></input>
                                     </div>
                                 </div>
                                 <div className="d-flex flex-row justify-content-around">
                                     <div className="input-label">
                                         <label htmlFor="IN">IN</label>
-                                        <input className="base input-sm" name="IN" type="number" min="1" max="100" value={this.state.base.in}></input>
+                                        <input className="base input-sm" name="IN" type="number" min="1" max="100" value={this.state.base.in} onChange={this.handleChange}></input>
                                     </div>
                                     <div className="input-label">
                                         <label htmlFor="ZT">ZT</label>
-                                        <input className="base input-sm" name="ZT" type="number" min="1" max="100" value={this.state.base.zt}></input>
+                                        <input className="base input-sm" name="ZT" type="number" min="1" max="100" value={this.state.base.zt} onChange={this.handleChange}></input>
                                     </div>
                                 </div>
                             </div>
@@ -192,9 +233,9 @@ class CharacterCreator extends Component {
 
                                     </div>
                                     <div className="input-label">
-                                        <label htmlFor="Ausb">AusB</label>
+                                        <label htmlFor="Ausb">B</label>
                                         <div className="d-flex flex-column justify-content-center span-container">
-                                            <span className="span-sm text-dark" name="Ausb"></span>
+                                            <span className="span-sm text-dark" name="Ausb">{c.calcB(this.state.race)}</span>
                                         </div>
 
                                     </div>
--- src/sys/calculations.js
+++ src/sys/calculations.js
@@ -39,3 +39,22 @@ exports.calcAu = (race) => {
     }
     return value;
 }
+
+exports.calcB = (race) => {
+    let value;
+    switch(race) {
+        case "gnome":
+            value = this.getRandom(3) + this.getRandom(3) + 8;
+            break;
+        case "halfling":
+            value = this.getRandom(3) + this.getRandom(3) + 8;
+            break;
+        case "dwarf":
+            value = this.getRandom(3) + this.getRandom(3) + this.getRandom(3) + 12;
+            break;
+        default:
+            value = this.getRandom(3) + this.getRandom(3) + this.getRandom(3) + this.getRandom(3) + 16;
+            break;
+    }
+    return value;
+}
